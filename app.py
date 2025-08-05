b3b99e9338e5dc9b241b092250052fb584a95463
Sam <105478252+samvictordr@users.noreply.github.com>
2025-08-05 16:42:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "CI/CD Pipeline Working Elegantly on Jenkins + Docker!"
+    return "CI/CD Pipeline Working on Jenkins + Docker! If you're seeing this page, it means updates have been automatically Served successfully."
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
