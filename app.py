cd02ea522385d22e0a9e882a5714e9eea1f0558f
Sam <105478252+samvictordr@users.noreply.github.com>
2025-08-05 19:12:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "CI/CD Pipeline Working on Jenkins + Docker! If you're seeing this page, it means updates have been automatically Served successfully."
+    return "CI/CD Pipeline Working on Jenkins + Docker! If you're seeing this page, it means updates have been automatically Served successfully. CICD Webhook trigger validation test count - 1"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
